4e15df7672cc3dff2e51a9e914fb5228a592eaa3
katekyy <deffkatek@gmail.com>
2022-09-04 18:17:46 +0200

    Ready. Set. Go!

---
+++ src/main.rs
@@ -0,0 +1,108 @@
+use ncurses::*;
+
+use std::{env, process::exit, io::BufRead};
+use std::{thread, time::Duration};
+
+use unicode_segmentation::UnicodeSegmentation;
+use regex::Regex;
+
+use termion::{color, style, terminal_size};
+
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    let (w, h) = termion::terminal_size().unwrap();
+
+    if args.len()-1 < 2 {
+        println!("{}{}err{}: Supplied {:?}/2 arguments!", color::Fg(color::Red), style::Bold, color::Fg(color::White), args.len()-1);
+        println!("{}{} | {}  'timer [message] [00:00:00]'", color::Fg(color::Red), style::Bold, color::Fg(color::White));
+        exit(1);
+    }
+
+    let re = Regex::new(r"^\d{2}:\d{2}:\d{2}$").unwrap();
+    assert!(re.is_match(&args[2]));
+
+    initscr();
+    noecho();
+
+    print_centered(((h/2)-4).try_into().unwrap(), args[1].clone());
+    print_centered((h-(h/8)).try_into().unwrap(), "press 'q' to exit".to_string());
+
+    start_countdown();
+
+    let mut quit = false;
+    while !quit {
+        let key = getch();
+
+        match key as u8 as char {
+            'q' => quit = true,
+             _  => {}
+        }
+    }
+
+    endwin();
+}
+
+fn start_countdown() {
+    // Countdown thread
+
+    thread::spawn(|| {
+        let args: Vec<_> = env::args().collect();
+        let (w, h) = termion::terminal_size().unwrap();
+
+        let raw: Vec<_> = args[2].split(':').collect();
+        let mut sec: i32 = raw[2].parse().unwrap();
+        let mut min: i32 = raw[1].parse().unwrap();
+        let mut hrs: i32 = raw[0].parse().unwrap();
+
+        // Countdown loop
+        
+        let mut broke = false;
+        while !broke {
+            print_centered(((h/2)-2).try_into().unwrap(), hrs.to_string()+"h "+&min.to_string()+"m "+ &sec.to_string()+"s");
+            thread::sleep(Duration::from_secs(1));
+
+            // Check if time has end or if someone is dumb and typed 00:00:00
+
+            if sec == 0 && min == 0 && hrs == 0 {
+                print_centered(((h/2)-2).try_into().unwrap(), "0h 0m 0s".to_string());
+                broke = true;
+            }
+
+            // Countdown logic
+
+            match sec as i32 {
+                0 => { 
+                    if  min != 0 {
+                        min -= 1; 
+                        sec += 60;
+                    }
+                },
+                _ => sec -= 1
+            }
+            match min as i32 {
+                0 => {
+                    if sec == 0 && min == 0 {
+                        if hrs == 0 {
+                            print_centered(((h/2)-2).try_into().unwrap(), "0h 0m 0s".to_string());
+                            broke = true;
+                        }
+
+                        hrs -= 1;
+                        min += 60;
+                    }
+                },
+                _ => {}
+            }
+        }  
+    });
+}
+
+fn print_centered(start_row: i32, text: String) {
+    let (w, h) = terminal_size().unwrap();
+    let half_len = UnicodeSegmentation::graphemes(&text as &str, true).count() / 2;
+    let adjusted_col = w/2 - half_len as u16;
+
+    mvprintw(start_row, adjusted_col as i32, &text as &str);
+    refresh();
+}
